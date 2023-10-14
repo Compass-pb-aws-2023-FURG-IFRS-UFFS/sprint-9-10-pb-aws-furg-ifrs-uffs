d9f9884cc20eef7b413b6cf006f37e6ed8d0726f
joaowinderfeldbussolotto <joaowinderfeldbussolotto@gmail.com>
2023-10-14 13:38:13 -0300

    :construction_worker: env: getting topic arn from ssm param

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -8,3 +8,9 @@ resource "aws_sns_topic_subscription" "developers_subscription" {
   protocol    = "email"
   endpoint    = element(var.subscriptions, count.index)
 }
+
+resource "aws_ssm_parameter" "developers-topic-arn"{
+  name = "uffs-bot-developers-topic-arn"
+  type = "String"
+  value = aws_sns_topic.snsTopic.arn
+}
