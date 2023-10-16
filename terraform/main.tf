02529201fe029b7eb88e2076dfebb07ff1da5843
Matheus Henrique <matheus1604.mh@gmail.com>
2023-10-16 14:20:29 -0300

    :construction_worker: env: adding backend for terraform statefile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "tofu-backend"              # Change this to your bucket name
+    key    = "terraform.tfstate" # Change this to the path of the state file inside the bucket
+    region = "us-east-1"                   # Change this to your region
+  }
+}
+
 provider "aws" {
   region = var.region
 
