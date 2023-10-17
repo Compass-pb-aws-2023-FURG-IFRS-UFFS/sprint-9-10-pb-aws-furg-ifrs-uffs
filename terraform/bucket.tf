c2080bbfe540628d1bd98d14b451dc0f8944ef5c
Matheus Henrique <matheus1604.mh@gmail.com>
2023-10-16 21:41:19 -0300

    :construction_worker: env: adding s3_bucket to terraform

--- terraform/bucket.tf
+++ terraform/bucket.tf
@@ -1,4 +1,4 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
+  bucket = var.bucket_lost_and_found_name
   tags = local.common_tags
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,12 +6,23 @@ variable "region" {
 variable "sns_topic_name" {
   description = "Name of the SNS topic to send messages to developers"
 }
-variable "subscriptions"{
-    description = "List of SNS subscriptions"
+variable "subscriptions" {
+  description = "List of SNS subscriptions"
 }
 
-variable "bucket_name" {
+variable "bucket_lost_and_found_name" {
   description = "Name of the S3 bucket to store the images"
-  default = "teste-terraform-2021-04-27"
+}
+
+variable "db_table_lost_and_found_name" {
+  description = "Name of the DynamoDB table to store the lost and found items"
+}
+
+variable "db_table_news_name" {
+  description = "Name of the DynamoDB table to store the news"
+}
+
+variable "db_table_intents_data_name" {
+  description = "Name of the DynamoDB table to store the intents data"
 }
 
