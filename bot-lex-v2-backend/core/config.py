eda3b2348501084c15213a56af64a37d5ef4ca7e
joaowinderfeldbussolotto <joaowinderfeldbussolotto@gmail.com>
2023-10-24 09:37:55 -0300

    :sparkles: feat: adding handler for get schedule intent

--- bot-lex-v2-backend/core/config.py
+++ bot-lex-v2-backend/core/config.py
@@ -4,5 +4,7 @@ class Settings:
     SNS_TOPIC_ARN = os.environ.get('SNS_TOPIC_ARN')
     CC_API_BASE_URL = os.environ.get('CC_API_BASE_URL')
     SIGN_IN_LAMBDA = os.environ.get('SIGN_IN_LAMBDA')
+    AUTH_API_URL = os.environ.get('AUTH_API_URL')
+    DYNAMO_DB_USERS_TABLE= os.environ.get('DYNAMO_DB_USERS_TABLE')
     
 settings = Settings()
