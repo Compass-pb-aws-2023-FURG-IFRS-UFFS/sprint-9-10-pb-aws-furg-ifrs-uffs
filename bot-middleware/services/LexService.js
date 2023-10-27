cf5f8c0630aaef5e4b082fc10372226efe282a8a
Josué Fernandes <josue7mf@hotmail.com>
2023-10-27 18:17:24 -0300

    :hammer: refactor: Alterando lex service

--- bot-middleware/services/LexService.js
+++ bot-middleware/services/LexService.js
@@ -1,17 +1,26 @@
-const LexException = require("../exceptions/aws-exceptions/LexException");
-const { BOT_ID, BOT_ALIAS_ID, LOCALE_ID } = require("../core/config");
-const TranscribeService = require("../../microservices/transcribe/services/TranscribeService");
-const PollyService = require("../../microservices/polly/services/PollyService");
-const {
-  LexRuntimeV2Client,
-  RecognizeTextCommand,
-} = require("@aws-sdk/client-lex-runtime-v2");
+const { BOT_ID, BOT_ALIAS_ID, LOCALE_ID, INITIAL_MESSAGE } = require("../core/config");
+const { LexRuntimeV2Client, RecognizeTextCommand } = require("@aws-sdk/client-lex-runtime-v2");
 
+/**
+ * Service class for interacting with Amazon Lex service.
+ */
 class LexService {
+
+  /**
+   * Constructor for LexService class.
+   * Initializes the LexRuntimeV2Client.
+   */
   constructor() {
     this.lexClient = new LexRuntimeV2Client();
   }
 
+  /**
+   * Sends a message to Amazon Lex and receives a response.
+   *
+   * @param {string} message - The message to be sent to the bot.
+   * @param {string} sessionId - The unique identifier for the user's session.
+   * @returns {Promise<string>} - A promise that resolves to the response from the bot.
+   */
   async sendMessage(message, sessionId) {
     const params = {
       botId: BOT_ID,
@@ -23,42 +32,16 @@ class LexService {
     const command = new RecognizeTextCommand(params);
     try {
       const returnLex = await this.lexClient.send(command);
-      let response = "";
-      console.log("Response from Amazon Lex V2:", returnLex);
       const intentName = returnLex.sessionState.intent.name;
-      console.log(intentName);
-      if (intentName == "Text-to-Speech") {
-        const sessionState = returnLex.sessionState.dialogAction.type;
-        console.log(sessionState);
-        if (sessionState == "ElicitSlot") {
-          response = returnLex.messages[0].content;
-        } else {
-          const pollyService = new PollyService();
-          response = await pollyService.textToSpeech(message);
-        }
-      } else if (intentName == "Speech-to-Text") {
-        const transcribeService = new TranscribeService();
-        response = await transcribeService.transcribeMessage(message);
-        console.log("Transcribe response:", response);
-      } else {
-        response = "Escolha um serviço:\n\n" +
-                   "1️⃣ Texto para Áudio   \n" +
-                   "2️⃣ Áudio para Texto   \n" +
-                   "3️⃣ Imagem para Texto   \n" +
-                   "4️⃣ Imagem para Áudio";
-        return returnLex.messages[0].content + "\n\n"  + response;
-      }
+
+      if(returnLex.messages) response = returnLex.messages[0].content;
+      if (intentName === "Hello") response += INITIAL_MESSAGE
+      
       return response;
     } catch (error) {
-      if (error.Code) {
-        const errorCode = error.Code;
-        throw LexException.handleLexException(errorCode);
-      } else {
-        console.error("Erro inesperado:", error);
-        return "Erro inesperado";
-      }
+      console.error("Unexpected error:", error);
+      return "Unexpected error occurred";
     }
-    // return returnLex
   }
 }
 
