fdc0d193e79babe2caebef7c391b46197dae6b7c
luiz-scarsi <116020373+LuizScarsi@users.noreply.github.com>
2023-10-27 20:47:21 -0300

    :sparkles: feat: adicionar rota do transcribe

--- microservices/transcribe/handler.js
+++ microservices/transcribe/handler.js
@@ -1,1 +1,6 @@
-let a = 1
+const transcribeService = require('./services/TranscribeService');
+
+function getTranscription(event, context, callback){
+    const transcribe = new transcribeService();
+    return transcribe.transcribeMessage(event);
+}
