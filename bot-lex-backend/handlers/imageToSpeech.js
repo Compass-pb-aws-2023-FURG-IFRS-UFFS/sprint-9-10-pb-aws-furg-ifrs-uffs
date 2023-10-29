0e83863baa7d89359a10c60f29b24e2424ed52f5
Josué Fernandes <josue7mf@hotmail.com>
2023-10-29 15:55:44 -0300

    :sparkles: feat: Adicionando handler imageToSpeech

--- bot-lex-backend/handlers/imageToSpeech.js
+++ bot-lex-backend/handlers/imageToSpeech.js
@@ -1,14 +1,23 @@
 const { isImage } = require("../helper/helper")
+const { textToSpeech } = require("./textToSpeech")
+const axios = require("axios")
+const { IMAGE_TO_TEXT_API } = require("../core/config")
 
 const imageToSpeech = async (message) => {
-  console.log("imageToSpeech");
-  let response = "This format is not acceptable, please try again and send an Image"
-  if(isImage(message)) {
-    // logic
-    response = message
+  try {
+    let response = "This format is not acceptable, please try again and send an Image"
+    if (isImage(message)) {
+      const url = new URLSearchParams(message).get("bucketKey")
+      const params = {
+        image: url
+      }
+      const data = await axios.post(IMAGE_TO_TEXT_API, params)
+      const text = data["data"]
+      response = await textToSpeech(text)
+    }
+    return response
+  } catch(error) {
+    throw new Error("An error ocurred in imageToSpeech")
   }
-  console.log(response);
-  return response
 }
-
 module.exports = { imageToSpeech }
