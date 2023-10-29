25527c185e1ba1fb864acdeed5f8c6dad674c7a1
RafaMPinheiro <rafaelmessipinheiro@gmail.com>
2023-10-29 15:36:53 -0300

    :hammer: refac: corrigir rota do rekognition

--- microservices/rekognition/handler.js
+++ microservices/rekognition/handler.js
@@ -1,4 +1,4 @@
-const RekognitionService = require("../services/RekognitionService");
+const RekognitionService = require("./services/RekognitionService");
 
 function handler(event, context, callback) {
   const image = JSON.parse(event.body).image;
