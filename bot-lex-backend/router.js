caf073f6a1474faa05a832ff1d608b7b4146c5e3
Josué Fernandes <josue7mf@hotmail.com>
2023-10-29 15:55:09 -0300

    :sparkles: feat: Adicionando router de intents

--- bot-lex-backend/router.js
+++ bot-lex-backend/router.js
@@ -1,1 +1,31 @@
-const a = 1
+const { textToSpeech } = require("./handlers/textToSpeech")
+const { speechToText } = require("./handlers/speechToText")
+const { imageToText } = require("./handlers/imageToText")
+const { imageToSpeech } = require("./handlers/imageToSpeech")
+const { prepareResponse } = require("./helper/helper")
+
+const intentHandlers = {
+  "Text-to-Speech": textToSpeech,
+  "Speech-to-Text": speechToText,
+  "Image-to-Text": imageToText,
+  "Image-to-Speech": imageToSpeech
+}
+const handler = async (event,context) => {
+  try {
+    const body = event['sessionState']['intent']
+    const intentName = body['name']
+    const messageFromUser = body["slots"]["texto"]["value"]["originalValue"]
+    if (!message || message == "") {
+      throw new Error("Invalid message format");
+    }
+    const messageToUser = await intentHandlers[intentName](messageFromUser)
+
+    return prepareResponse(event, messageToUser)
+  } catch(error) {
+    return prepareResponse(event, "An Error Ocurred in amazon lex")
+  }
+}
+
+module.exports = { handler }
+
+
