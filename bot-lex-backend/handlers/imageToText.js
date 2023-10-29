fe38ef996e0d44b3abe0b17b3449db5051ee082c
Josué Fernandes <josue7mf@hotmail.com>
2023-10-29 15:56:07 -0300

    :sparkles: feat: Adicionando handler imageToText

--- bot-lex-backend/handlers/imageToText.js
+++ bot-lex-backend/handlers/imageToText.js
@@ -1,14 +1,22 @@
 const { isImage } = require("../helper/helper")
+const axios = require("axios")
+const { IMAGE_TO_TEXT_API } = require("../core/config")
 
 const imageToText = async (message) => {
-  console.log("imageToText");
-  let response = "This format is not acceptable, please try again and send an Image"
-  if(isImage(message)) {
-    // logic
-    response = message
+  try {
+    let response = "This format is not acceptable, please try again and send an Image"
+    if(isImage(message)) {
+      const url = new URLSearchParams(message).get("bucketKey")
+      const params = {
+        image: url
+      }
+      const data = await axios.post(IMAGE_TO_TEXT_API, params)
+      response = data["data"]
+    }
+    return response
+  } catch(error) {
+    throw new Error("An error ocurred in imageToText")
   }
-  console.log(response);
-  return response
 }
 module.exports = { imageToText }
 
